7d4ac85598650dcc896df6ad55e533a8733600f2
lalit452 <lalitkumarmeena452@gmail.com>
2025-03-19 06:31:52 +0530

    update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,15 @@
-import axios from "axios";
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//     baseURL : "https://realtime-chat-app-backend-psi.vercel.app/api",
+//     withCredentials: true, 
+// })   
+
 
 export const axiosInstance = axios.create({
-    baseURL : "https://realtime-chat-app-backend-psi.vercel.app/api",
-    withCredentials: true, 
-})   
+    baseURL: "https://realtime-chat-app-backend-psi.vercel.app/api",
+    withCredentials: true,
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+    }
+});
