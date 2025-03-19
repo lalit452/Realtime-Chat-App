6328aeb4dabe2c12fed8ebba8608b13fbb723bba
lalit452 <lalitkumarmeena452@gmail.com>
2025-03-19 05:57:39 +0530

    updated backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,31 +1,37 @@
-// const express = require("express")
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.route.js"
 import cors from "cors";
-
 import {connectDB} from './lib/db.js'
-dotenv.config()
+
+
 const app = express();
 
 const PORT = process.env.PORT
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
-// this will allow you to extract json data out of body
+
 
 app.use(express.json()); 
 app.use(cookieParser());
 app.use(cors({ 
     origin: FRONTEND_URL,
-    credentials: true
-}))
+    credentials: true,
+}));
 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+connectDB()
 
-app.listen(PORT, ()=>{
-    console.log("server is running on PORT :" + PORT);
-    connectDB()
-}) 
+
+// app.listen(PORT, ()=>{
+//     console.log("server is running on PORT :" + PORT);
+// }) 
+
+if (process.env.NODE_ENV !== "production") {
+    app.listen(PORT, () => {
+      console.log("Server is running on PORT: " + PORT);
+    });
+  }
